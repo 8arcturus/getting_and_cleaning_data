be0da39cb5dcdd5aa554800142dd26483066b536
SAKTHI SIVRAM <psakthisivram@gmail.com>
2016-06-14 23:51:04 -0400

    variable name

--- run_analysis.R
+++ run_analysis.R
@@ -5,7 +5,7 @@ filename <- "UCI_HAR_Dataset.zip"
 
 # Download dataset
 if (!file.exists(filename)){
-  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   require(downloader)
   download(fileUrl, filename, mode = "wb")
 }  
